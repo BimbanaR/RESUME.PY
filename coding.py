3e4f0c2988d72efeda3ce6754211ec135bee2186
BimbanaR <bimbana.r@gmail.com>
2025-03-29 04:22:24 -1200

    added project files
    added the datafile as well

---
+++ coding.py
@@ -0,0 +1,41 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Personal/AppData/Local/Programs/Python/Python312/RESUME.py
+Bimbana R
+Phone number：7899505240 E-mail：bimbana.r@gmail.com
+Location： Tumkur, India
+
+CAREER OBJECTIVE
+I’m really passionate about making a positive impact and delivering results. I love working in dynamic
+environments and enjoy the challenges that come with managing teams and projects. I’m confident that my
+strong organizational skills and ability to multitask will be valuable assts in a management role.
+EDUCATION
+ Secondary education Maruthi international School 66% (10th grade) 2021-22
+ Higher secondary education Sri Sapthagiri PU college 79% (12th grade) 2023-24
+ BCA Vidyavahini first grade college present
+EXPERIENCES
+ Class monitor- Maruthi international School
+Served as a class monitor multiple times and maintained a disciplined classroom
+ Leader- Sri Sapthagiri PU College
+Lead a team of 6 members for a project “BOCS-Business Oriented Communication Skills” where we
+surveyed 100+ people about ‘Philips’, analysed and interpreted the data and presented it.
+ Leader- Sri Sapthagiri PU College
+Lead a team of 5 members for a project “SEPME-Sapthagiri Experiential Program for Management and
+Entrepreneurship” where we came up with an innovative startup business idea of a bakery and presented it.
+ Captain- Sri Sapthagiri PU college
+Captained a team of 12 members and took the responsibilities of organising practices and many more
+CERTIFICATES
+ SEPME  RUNNING  THALUK LEVEL HANDBALL  BOCS
+
+ DIGITAL 101 JOURNEY  MICROSOFT LINKEDIN
+CORE COMPETENCIES
+ Leadership and teamwork
+ Time management
+ Adaptability and learning
+ Communication skills
+ Interpersonal skills
+HOBBIES
+ Dancing  Hiking  Baking
+DECLARATION
+All the information provided in this resume is true to the best of my knowledge and belief
